a46035bc1ca7a86e228908cea85023950ab8312c
jday151r <j.day151r@gmail.com>
2020-07-22 21:03:00 -0700

    pre-refactor weapon system

---
+++ Assets/Editor/WeaponScriptableObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScriptableObject : ScriptableObject
+{
+    public float damage;
+    public float lifeTime;
+    public AudioClip spawnSFX;
+}
+
+[CreateAssetMenu(fileName = "NewProjectileWeapon", menuName = "Weapon/ProjectileWeapon", order = 1)]
+public class ProjectileWeapon : WeaponScriptableObject
+{
+    public float velocity;
+}
